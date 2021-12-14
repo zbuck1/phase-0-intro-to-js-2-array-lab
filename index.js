d0b2a22002ea6082dd375b3a16be03ff7ca67e98
zbuck1 <95191417+zbuck1@users.noreply.github.com>
2021-12-14 11:04:12 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(`${name}`)
+    }
+    function destructivelyPrependCat(name) {
+        cats.unshift(`${name}`)
+    }
+    function destructivelyRemoveLastCat(name) {
+        cats.pop(`${name}`)
+    }
+    function destructivelyRemoveFirstCat(name) {
+        cats.shift(`${name}`)
+    }
+
+    function appendCat() {
+        const appendCat = [...cats, "Broom"]
+        return appendCat
+    }
+    function prependCat() {
+        const prependCat = ["Arnold", ...cats]
+        return prependCat
+    }
+    function removeLastCat(slice) {
+        const removeLastCat = cats.slice(0,-1)
+    }
+
+
+
+
